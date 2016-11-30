72554becca8a37f1f298228b7f7380ce685a9c3b
Vivian <expandwings@live.cn>
2016-11-29 23:01:36 -0800

    rename

--- learner.py
+++ learner.py
@@ -2,7 +2,7 @@
 import optparse, sys, os, bleu, random, math, string, copy, itertools, random
 from collections import namedtuple, defaultdict
 
-def get_candidates(nbest, target, source):
+def get_nbest(nbest, target, source):
     ref = [line.strip().split() for line in open(target).readlines()]
     src = [line.strip().split() for line in open(source).readlines()]
     candidates = [line.strip().split("|||") for line in open(nbest).readlines()]
@@ -17,7 +17,6 @@ def get_candidates(nbest, target, source):
         nbests[i].append(translation(features, bleu.smoothed_bleu(stats)))
     return nbests
 
-# Save time by not caring if s1 > s2 since they're random.
 def get_samples(nbest, tau, alpha):
     random.seed()
     for _ in xrange(tau):
@@ -26,12 +25,12 @@ def get_samples(nbest, tau, alpha):
         if (s1 != s2) and (math.fabs(s1.smoothed_bleu - s2.smoothed_bleu) > alpha):
             yield (s1, s2) if s1.smoothed_bleu > s2.smoothed_bleu else (s2, s1)
 
-def perceptron(nbests, epochs, tau, eta, xi, alpha):
+def computePRO(nbests, epochs, tau, eta, xi, alpha):
     theta = [random.uniform(-1.0*sys.maxint, 1.0) for _ in xrange(len(nbests[0][0].features))]
-    for e in xrange(epochs):
+    for epoch in xrange(epochs):
         mistakes = 0.0
-        observed = 0.0
-        sys.stderr.write("Starting iteration %s..." % (e+1))
+        num = 0.0
+        sys.stderr.write("Starting iteration %s..." % (epoch+1))
         for nbest in nbests:
             for (s1, s2) in sorted(get_samples(nbest, tau, alpha), key=lambda (s1, s2): s2.smoothed_bleu - s1.smoothed_bleu)[:xi]:
                 if sum([x * y for x, y in zip(theta, s1.features)]) <= sum([x * y for x, y in zip(theta, s2.features)]):
@@ -41,10 +40,10 @@ def perceptron(nbests, epochs, tau, eta, xi, alpha):
                     update = [item * eta for item in [x - y for x, y in zip(s1.features, s2.features)]]
                     # theta = theta + update
                     theta = [x + y for x, y in zip(theta, update)]# theta += eta * (s1.features - s2.features)
-                observed += 1
+                num += 1
         sys.stderr.write("\n")
         sys.stderr.write("Mistakes --> %s...\n" % str(int(mistakes)))
-        theta = [t/(observed) for t in theta]
+        theta = [t/(num) for t in theta]
     sys.stderr.write("\n")
     return theta
 
@@ -59,6 +58,6 @@ if __name__ == '__main__':
     optparser.add_option("-r", "--eta", dest="eta", default=0.1, type="float", help="Perceptron learning rate (default=0.1)")
     optparser.add_option("-i", "--epochs", dest="epochs", default=5, type="int", help="Number of epochs for perceptron training (default=5)")
     (opts, _) = optparser.parse_args()
-    nbests = get_candidates(opts.nbest, opts.target, opts.source)
-    weights = perceptron(nbests, opts.epochs, opts.tau, opts.eta, opts.xi, opts.alpha)
+    nbests = get_nbest(opts.nbest, opts.target, opts.source)
+    weights = computePRO(nbests, opts.epochs, opts.tau, opts.eta, opts.xi, opts.alpha)
     print "\n".join([str(weight) for weight in weights])
